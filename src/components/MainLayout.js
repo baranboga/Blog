8aba49d002a0943e9219a51a4f341e57a94f60a0
Baran <baraboga97@gmail.com>
2023-08-23 20:30:56 +0300

    asd

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,61 +1,105 @@
-import React, { useEffect, useState } from 'react'
-import $ from 'jquery';
-import { useSelector } from 'react-redux'
-import { Link, NavLink, Outlet, useNavigate } from 'react-router-dom'
-import { selectKeyword, } from '../requests/slice';
-import { FaShoppingBasket } from 'react-icons/fa';
-import { FaSignOutAlt } from 'react-icons/fa';
-import { FaUser } from 'react-icons/fa';
-import logo from '../images/3.jpeg';
-
-
-
-
-
-
-
-
+import React, { useEffect, useState } from "react";
+import $ from "jquery";
+import { useSelector } from "react-redux";
+import { Link, NavLink, Outlet, useNavigate } from "react-router-dom";
+import { selectKeyword } from "../requests/slice";
+import { FaShoppingBasket } from "react-icons/fa";
+import { FaSignOutAlt } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import logo from "../images/3.jpeg";
 
 export default function MainLayout() {
-    const [key, setKey] = useState()
-    let navigate = useNavigate()
+  const [key, setKey] = useState();
+  let navigate = useNavigate();
 
 
-    useEffect(()=>{
-        let admin = localStorage.getItem('admin');
-        setKey(admin)
-       
+  a
 
-        $(document).ready(function(){
-
-            /* Mobile menu */
-            $('.mobile-menu-icon').click(function(){
-                $('.templatemo-nav').slideToggle();				
-            });
-        
-            $( window ).resize(function() {
-                if($( window ).width() > 767) {
-                    $('.templatemo-nav').show();	
-                } else {
-                    $('.templatemo-nav').hide();	
-                } 
-            });
-        
-          // http://stackoverflow.com/questions/2851663/how-do-i-simulate-a-hover-with-a-touch-in-touch-enabled-browsers
-          $('body').bind('touchstart', function() {});
-        
-        });
-    })
-  
-
-
-
-    return (
-
-
-
-        <div>
-            <div className="templatemo-container">
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">
+            Navbar
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            {/* nav-link */}
+            <ul class="navbar-nav">
+              <li>
+                <Link activeClassName="active" to="/"
+                style={{color:"black",textDecoration:"none",marginRight:"10px"}}>
+                  Anasayfa
+                </Link>
+              </li>
+              <li>
+                <Link className="navbar-nav" to="/about"
+                 style={{color:"black",textDecoration:"none",marginRight:"10px"}}>
+                  Hakkımda
+                </Link>
+              </li>
+              <li>
+                <Link activeClassName="active" to="/blog"
+                 style={{color:"black",textDecoration:"none",marginRight:"10px"}}>
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link activeClassName="active" to="/contact"
+                 style={{color:"black",textDecoration:"none",marginRight:"10px"}}>
+                  İletişim
+                </Link>
+              </li>
+              {key && (
+                <li>
+                  <NavLink
+                     style={{color:"black",textDecoration:"none",marginRight:"10px"}}
+                    to="/admin"
+                  >
+                    Admin
+                  </NavLink>
+                </li>
+              )}
+              {key && (
+                <li>
+                  <NavLink
+                    style={{color:"black",textDecoration:"none",marginRight:"10px"}}
+                    to="/bloglist"
+                  >
+                    Bloglistesi
+                  </NavLink>
+                </li>
+              )}
+              {key && (
+                <li>
+                  <NavLink
+                    activeClassName="active"
+                    onClick={() => {
+                      localStorage.clear();
+                      navigate("/");
+                    }}
+                    style={{color:"black",textDecoration:"none"}}
+                    to={"/"}
+                  >
+                    Çıkış
+                  </NavLink>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </nav>
+      {/* <div className="templatemo-container">
                 <div className="templatemo-block-left">
                     <div className="templatemo-header-left">
                         <div className="templatemo-header-text-wrap">
@@ -82,10 +126,8 @@ export default function MainLayout() {
                         </div>
                         <nav className="templatemo-nav">
                             <ul className="text-uppercase">
-                                <li><NavLink activeClassName="active" style={{ fontSize: "15px", textDecoration:"none" }} to="/">Anasayfa</NavLink></li>
-                                <li><NavLink activeClassName="active" style={{ fontSize: "15px", textDecoration:"none"  }} to="/about">Hakkımda</NavLink></li>
-                                <li><NavLink activeClassName="active" style={{ fontSize: "15px", textDecoration:"none"  }} to="/blog">Blog</NavLink></li>
-                                <li><NavLink activeClassName="active" style={{ fontSize: "15px", textDecoration:"none"  }} to="/contact">İletişim</NavLink></li>
+                               
+                            
                                 {key&&<li><NavLink style={{ fontSize: "15px", textDecoration:"none"  }} to="/admin">Admin</NavLink></li>}
                                 {key&&<li><NavLink style={{ fontSize: "15px", textDecoration:"none"  }} to="/bloglist">Bloglistesi</NavLink></li>}
                                 {key&&<li><NavLink activeClassName="active" onClick={(()=>{
@@ -100,35 +142,29 @@ export default function MainLayout() {
                         </nav>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
+      <Outlet />
 
-            <Outlet />
-
-            
-            <div className="templatemo-dark-bg">
-                <div className="container" />
-                <div className="row">
-                    <div className="col-lg-12 text-center">
-                   
-                        <hr className="templatemo-about-hr" />
-                        <p className="gray-text about-us-text" style={{fontFamily:'cursive',fontSize:"1.5rem"}}>serius est quam cogitas...</p>
-                    </div>
-                </div>
-            </div>
-            <div />
-
-
-            <footer className="text-center">
-                <p className="text-uppercase">
-                    Copyright &copy; Rıza Baran Boğa
-                </p>
-            </footer>
-
-
-
-
+      <div className="templatemo-dark-bg">
+        <div className="container" />
+        <div className="row">
+          <div className="col-lg-12 text-center">
+            <hr className="templatemo-about-hr" />
+            <p
+              className="gray-text about-us-text"
+              style={{ fontFamily: "cursive", fontSize: "1.5rem" }}
+            >
+              serius est quam cogitas...
+            </p>
+          </div>
         </div>
+      </div>
+      <div />
 
-    )
+      <footer className="text-center">
+        <p className="text-uppercase">Copyright &copy; Rıza Baran Boğa</p>
+      </footer>
+    </div>
+  );
 }
